87ec11ce93141986ff2dc0eb8cf3465a828ed46d
JuanSebastianPC <>
2019-05-11 22:16:45 -0500

    Inicio del Proyecto Colegio.

---
+++ src/ConexionMysql/Prueba.java
@@ -0,0 +1,20 @@
+
+package ConexionMysql;
+
+/**
+ *
+ * @author Lasso
+ */
+public class Prueba {
+    
+    
+    
+    public static void main(String[] args) {
+        
+        ConexionMysql c = new ConexionMysql();
+        
+        c.conectar(); 
+    }
+    
+    
+}
